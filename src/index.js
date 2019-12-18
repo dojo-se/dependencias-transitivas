1e72f9195956653b15a03b9d30f05c756c439a87
Eduardo Borges <euduardoborges@gmail.com>
2019-12-18 20:25:07 -0300

    chore: secound test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 
 
-export const fn = ()=> true;
+export const input = `
+A   B   C
+B   C   E
+C   G
+D   A   F
+E   F
+F   H
+`;
+
+export const output = `
+A   B C E F G H
+B   C E F G H
+C   G
+D   A B C E F G H
+E   F H
+F   H
+`;
+
+
+
+export const main = () => output;
+
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,16 @@
-// @jest
+
+import { input, output, main } from './index'
+
 
 describe("@", ()=> {
   test("Teste inicial", ()=> {
     expect(true).toEqual(true);
   })
+
+  test("Espera a saída correta", () => {
+    const expectResult = main();
+    expect(expectResult).toEqual(output);
+  })
+
+
 })
